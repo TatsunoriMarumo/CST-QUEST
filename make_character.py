6a86be2c340beb04f2059a2ec6db41ecb04ac1ab
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-15 17:42:10 -0800

    create make_character() function

---
+++ make_character.py
@@ -0,0 +1,22 @@
+from choose_class import choose_class
+
+"""
+Tatsunori Marumo
+A01327744
+"""
+
+
+def create_character():
+    user_name = input("enter your name")
+    user_class = choose_class()
+    if user_class == "front_end_developer":
+        character = {"name": user_name, "class": user_class, "level": 1, "status": {}, "coordinates": [0, 0],
+                     "inventory": []}
+    elif user_class == "back_end_developer":
+        character = {"name": user_name, "class": user_class, "level": 1, "status": {}, "coordinates": [0, 0],
+                     "inventory": []}
+    else:
+        character = {"name": user_name, "class": user_class, "level": 1, "status": {}, "coordinates": [0, 0],
+                     "inventory": []}
+
+    return character
