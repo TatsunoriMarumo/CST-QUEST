360bde457d7a39e5fef7168cdd90b854fd4ed394
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-18 16:39:53 -0800

    create dogstring for choose_class.py

--- choose_class.py
+++ choose_class.py
@@ -5,10 +5,21 @@ A01327744
 
 
 def choose_class():
+    """
+    Let player choose a class
+
+    This function ask player to choose one class from three classes
+
+    postcondition: no invalid input is accepted
+    :return: the chosen user class stored as string
+    """
+
     while True:
-        user_class = input("choose a class\n1: front-end developer\n2: back-end developer\n3: full-stack developer")
+        user_class = input("choose a class\n1: front-end developer\n2: back-end developer\n3: full-stack developer\n")
         if user_class in ["1", "2", "3"]:
             break
+        else:
+            print("Invalid input. Please choose a valid class number.")
     if user_class == "1":
         user_class = "front_end_developer"
     elif user_class == "2":
--- make_character.py
+++ make_character.py
@@ -10,7 +10,8 @@ def create_character():
     user_name = input("enter your name")
     user_class = choose_class()
     if user_class == "front_end_developer":
-        character = {"name": user_name, "class": user_class, "level": 1, "status": {}, "coordinates": [0, 0],
+        character = {"name": user_name, "class": user_class, "level": 1, "status": {"max_hp": 0, "current_hp": 0,
+                     "intelligence": 0, "luck": 0, "typing_speed": 0, "mentality": 0}, "coordinates": [0, 0],
                      "inventory": []}
     elif user_class == "back_end_developer":
         character = {"name": user_name, "class": user_class, "level": 1, "status": {}, "coordinates": [0, 0],
