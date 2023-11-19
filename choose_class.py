7f947dbb0b90b406ecdbb77e925330f7e1d05182
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-18 17:02:38 -0800

    write first draft choose_class()

--- choose_class.py
+++ choose_class.py
@@ -18,15 +18,17 @@ def choose_class():
 
     while True:
         user_class = input("choose a class\n1: front-end developer\n2: back-end developer\n3: full-stack developer\n")
-        if user_class in ["1", "2", "3"]:
-            break
-        else:
-            print("Invalid input. Please choose a valid class number.")
-    if user_class == "1":
-        user_class = "front_end_developer"
-    elif user_class == "2":
-        user_class = "back_end_developer"
-    elif user_class == "3":
-        user_class = "full_stack_developer"
+        if user_class not in ["1", "2", "3"]:
+            raise ValueError
+        try:
+            if user_class == "1":
+                return "front_end_developer"
+            elif user_class == "2":
+                return "back_end_developer"
+            elif user_class == "3":
+                return "full_stack_developer"
+        except ValueError:
+            print("Invalid input. Please choose a valid class number")
 
-    return user_class
+
+choose_class()
