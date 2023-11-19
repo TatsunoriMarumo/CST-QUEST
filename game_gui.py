33e9564cc4418699b376bd37c8ba4e53735c5c53
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-18 20:42:32 -0800

    initiate game_gui.py

---
+++ game_gui.py
@@ -0,0 +1,26 @@
+"""
+Tatsunori Marumo
+A01327744
+"""
+
+import tkinter
+from tkinter import Pack, filedialog, Text
+import os
+import pathlib
+
+root = tkinter.Tk()
+root.title("CST QUEST")
+canvas = tkinter.Canvas(width=500, height=500, bg="#123456")
+canvas.pack()
+
+
+
+root.mainloop()
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
