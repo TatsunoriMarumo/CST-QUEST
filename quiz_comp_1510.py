868ed53a15735c802c08e3f8b88693b9b7d2ef1b
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-29 18:26:26 -0800

    Create use_quiz_week_four()

--- quiz_comp_1510.py
+++ quiz_comp_1510.py
@@ -6,7 +6,7 @@ A01366848
 """
 
 
-def use_quiz_week_1():
+def use_quiz_week_one():
     while True:
         user_answer = input("Which ONE of the following is NOT a Python keyword?\n"
                             "1: in\n2: continue\n3: assert\n4: false\n")
@@ -16,7 +16,7 @@ def use_quiz_week_1():
             return user_answer == "4"
 
 
-def use_quiz_week_2():
+def use_quiz_week_two():
     while True:
         user_answer = input("The flowchart symbol for making a choice is:\n"
                             "1: A sheared parallelogram\n2: A diamond\n3: A pill-shaped oval\n4: A rectangle\n")
@@ -26,7 +26,7 @@ def use_quiz_week_2():
             return user_answer == "2"
 
 
-def use_quiz_week_3():
+def use_quiz_week_three():
     while True:
         user_answer = input('If word = "I love Fridays!", what is word[-3]?\n'
                             '1: This will cause an interpreter error\n2: "a"\n3: "y"\n4: "o"\n')
@@ -34,6 +34,22 @@ def use_quiz_week_3():
             raise ValueError("Please answer with number between 1 to 4")
         else:
             return user_answer == "3"
+
+
+def use_quiz_week_four():
+    while True:
+        user_answer = input('What word do we use to describe two variables that are bound to the same object in the heap?\n'
+                            'concatenated\nimmutable\nreferences\naliases')
+
+        if user_answer not in ["1", "2", "3", "4"]:
+            raise ValueError("Please answer with number between 1 to 4")
+        else:
+            return user_answer == "4"
+
+
+def use_quiz_week_five():
+    while True:
+
 
 
 
