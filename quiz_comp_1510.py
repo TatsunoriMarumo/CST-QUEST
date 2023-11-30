9f30778e1127597991fb4b11f02df085e396fa35
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-30 11:43:19 -0800

    Create use_quiz_week_five() and use_quiz_week_six()

--- quiz_comp_1510.py
+++ quiz_comp_1510.py
@@ -7,48 +7,75 @@ A01366848
 
 
 def use_quiz_week_one():
-    while True:
-        user_answer = input("Which ONE of the following is NOT a Python keyword?\n"
-                            "1: in\n2: continue\n3: assert\n4: false\n")
-        if user_answer not in ["1", "2", "3", "4"]:
-            raise ValueError("Please answer with number between 1 to 4")
-        else:
-            return user_answer == "4"
+    user_answer = input("Which ONE of the following is NOT a Python keyword?\n"
+                        "1: in\n2: continue\n3: assert\n4: false\n")
+    if user_answer not in ["1", "2", "3", "4"]:
+        raise ValueError("Please answer with number between 1 and 4")
+    else:
+        return user_answer == "4"
 
 
 def use_quiz_week_two():
-    while True:
-        user_answer = input("The flowchart symbol for making a choice is:\n"
-                            "1: A sheared parallelogram\n2: A diamond\n3: A pill-shaped oval\n4: A rectangle\n")
-        if user_answer not in ["1", "2", "3", "4"]:
-            raise ValueError("Please answer with number between 1 to 4")
-        else:
-            return user_answer == "2"
+    user_answer = input("The flowchart symbol for making a choice is:\n"
+                        "1: A sheared parallelogram\n2: A diamond\n3: A pill-shaped oval\n4: A rectangle\n")
+    if user_answer not in ["1", "2", "3", "4"]:
+        raise ValueError("Please answer with number between 1 and 4")
+    else:
+        return user_answer == "2"
 
 
 def use_quiz_week_three():
-    while True:
-        user_answer = input('If word = "I love Fridays!", what is word[-3]?\n'
-                            '1: This will cause an interpreter error\n2: "a"\n3: "y"\n4: "o"\n')
-        if user_answer not in ["1", "2", "3", "4"]:
-            raise ValueError("Please answer with number between 1 to 4")
-        else:
-            return user_answer == "3"
+    user_answer = input('If word = "I love Fridays!", what is word[-3]?\n'
+                        '1: This will cause an interpreter error\n2: "a"\n3: "y"\n4: "o"\n')
+    if user_answer not in ["1", "2", "3", "4"]:
+        raise ValueError("Please answer with number between 1 and 4")
+    else:
+        return user_answer == "3"
 
 
 def use_quiz_week_four():
-    while True:
-        user_answer = input('What word do we use to describe two variables that are bound to the same object in the heap?\n'
-                            'concatenated\nimmutable\nreferences\naliases')
-
-        if user_answer not in ["1", "2", "3", "4"]:
-            raise ValueError("Please answer with number between 1 to 4")
-        else:
-            return user_answer == "4"
+    user_answer = input('What word do we use to describe two variables that are bound to the same object in the heap?\n'
+                        'concatenated\nimmutable\nreferences\naliases')
+    if user_answer not in ["1", "2", "3", "4"]:
+        raise ValueError("Please answer with number between 1 and 4")
+    else:
+        return user_answer == "4"
 
 
 def use_quiz_week_five():
-    while True:
+    user_answer = input("Which ONE of these reads the tokens generated from our source code using syntax rules?\n"
+                        "1: compiler\n2: parser\n3: interpreter\n4: lever")
+    if user_answer not in ["1", "2", "3", "4"]:
+        raise ValueError("Please answer with number between 1 and 4")
+    else:
+        return user_answer == "2"
+
+
+def use_quiz_week_six():
+    user_answer = input("What does the try block in a try-except statement do?"
+                        "1: It runs a block of code and handles any exceptions that are thrown"
+                        "2: It tries different blocks of code until one without an error is found"
+                        "3: It attempts to execute a block of code and retries if an error occurs"
+                        "4: It tries to execute a block of code and exits if it fails.")
+    if user_answer not in ["1", "2", "3", "4"]:
+        raise ValueError("Please answer with number between 1 and 4")
+    else:
+        return user_answer == "1"
+
+    def use_quiz_weak_seven():
+
+
+
+def use_bonus_quiz():
+    user_answer = input("Chris is a good teacher\n1: True\n2: False")
+    if user_answer not in ["1", "2"]:
+        raise ValueError("Please answer with number between 1 to 2")
+    elif user_answer == "1":
+        return True
+    else:
+        # die()
+
+
 
 
 
@@ -60,7 +87,14 @@ def use_quiz_week_five():
 
 
 def main():
-    use_quiz_week_1()
+    while True:
+        try:
+            a = use_quiz_week_one()
+        except ValueError as e:
+            print(e)
+        else:
+            print(a)
+            return
 
 
 if __name__ == '__main__':
