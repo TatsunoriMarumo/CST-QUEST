a2880b664628726d840b2d85e555f008a50ee0a0
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 14:43:53 -0800

    polish determine_turn_order() function

--- CST_Quest/combat/determine_turn_order.py
+++ CST_Quest/combat/determine_turn_order.py
@@ -4,12 +4,13 @@ A01327744
 Ephraim Hsu
 A01366848
 """
+import random
 
 
 def determine_turn_order(character, foe):
-    player_speed = character["stats"]["typing_speed"]
-    foe_speed = foe["stats"]["typing_speed"]
-    if player_speed > foe_speed:
+    if character["stats"]["typing_speed"] > foe["stats"]["typing_speed"]:
         return "player"
+    elif character["stats"]["typing_speed"] == foe["stats"]["typing_speed"]:
+        return random.choice(["player", "foe"])
     else:
         return "foe"
