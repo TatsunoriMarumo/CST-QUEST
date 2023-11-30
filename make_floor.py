8e955c37b6a056650e879d8d070c3848b898e978
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-29 17:46:04 -0800

    Create make_board() function

--- make_floor.py
+++ make_floor.py
@@ -7,17 +7,18 @@ A01366848
 import random
 
 
-def make_floors():
+def make_board():
     """
     Create a virtual game board.
     """
 
-    room_types = ["enemy", "puzzle", "event", "loot"]
-    "generate random number, if it is in range, assign room_types[0], room_types[1]"
-
-    number_of_floors = 6
-    return {(floor, row, column): f"{random.choice(room_types)} room"
-            for floor in range(1, number_of_floors + 1) for row in range(0, 5) for column in range(0, 5)}
+    game_board = {(row, column): random.choice(range(5)) for row in range(11) for column in range(11)}
+    print(game_board)
 
 
-print(make_floors())
+def main():
+    make_board()
+
+
+if __name__ == "__main__":
+    main()
