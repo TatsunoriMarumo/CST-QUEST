a2c32fdc667081928f5fe5fa3c0b78abbec74bd9
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-11-29 18:22:03 -0800

    Create use_quiz_week_3

--- quiz_comp_1510.py
+++ quiz_comp_1510.py
@@ -9,21 +9,37 @@ A01366848
 def use_quiz_week_1():
     while True:
         user_answer = input("Which ONE of the following is NOT a Python keyword?\n"
-                            "1: in\n2: continue\n3: assert\n4: false")
+                            "1: in\n2: continue\n3: assert\n4: false\n")
         if user_answer not in ["1", "2", "3", "4"]:
             raise ValueError("Please answer with number between 1 to 4")
         else:
-            return user_answer == "1"
+            return user_answer == "4"
 
 
 def use_quiz_week_2():
     while True:
         user_answer = input("The flowchart symbol for making a choice is:\n"
-                            "1: A sheared parallelogram\n2: A diamond\n3: A pill-shaped oval\n4: A rectangle")
+                            "1: A sheared parallelogram\n2: A diamond\n3: A pill-shaped oval\n4: A rectangle\n")
         if user_answer not in ["1", "2", "3", "4"]:
             raise ValueError("Please answer with number between 1 to 4")
         else:
             return user_answer == "2"
+
+
+def use_quiz_week_3():
+    while True:
+        user_answer = input('If word = "I love Fridays!", what is word[-3]?\n'
+                            '1: This will cause an interpreter error\n2: "a"\n3: "y"\n4: "o"\n')
+        if user_answer not in ["1", "2", "3", "4"]:
+            raise ValueError("Please answer with number between 1 to 4")
+        else:
+            return user_answer == "3"
+
+
+
+
+
+
 
 
 
