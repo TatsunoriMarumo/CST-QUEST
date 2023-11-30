140d4e6b54d2a8c0fac61d03acd971eb5028c437
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 13:58:07 -0800

    push work before creating packages

---
+++ foe_function.py
@@ -0,0 +1,20 @@
+"""
+Tatsunori Marumo
+A01327744
+Ephraim Hsu
+A01366848
+"""
+import random
+import determine_turn_order
+
+
+def fight_foe(character, foe):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
