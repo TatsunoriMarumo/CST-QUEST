443d515b9dbf36476793e274fd6c0f4db5b1dc1d
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 15:35:05 -0800

    save work

--- CST_Quest/combat/calculate_damage.py
+++ CST_Quest/combat/calculate_damage.py
@@ -7,15 +7,5 @@ A01366848
 import random
 
 
-def calculate_damage(source_entity, source_action, recipient_entity):
-    recipient_block = recipient_entity["stats"]["mentality"]
-    source_attack = source_entity["stats"]["intelligence"]
-    source_defense = source_entity["stats"]["mentality"]
-    light_attack_range = random.choice(range(source_attack - 1, source_attack + 2))
-    heavy_attack_range = random.choice(range(source_attack * 1.2 - 4, source_defense * 1.2 + 1))
-    if source_action == "light_attack":
-        recipient_entity["stats"]["current_hp"] -= light_attack_range - recipient_block
-    elif source_action == "heavy_attack":
-        recipient_entity["stats"]["current_hp"] -= heavy_attack_range - recipient_block
-    elif source_action == "unique_skill":
-        pass
+def calculate_attack_vs_defense(attacking, attack_type, defending):
+    pass
