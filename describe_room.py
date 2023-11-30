a2ec9612611c9d98ba5ea6443231883a975f9dac
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-29 17:57:03 -0800

    polish describe_room() function

--- describe_room.py
+++ describe_room.py
@@ -7,7 +7,7 @@ A01366848
 
 
 def describe_room(character, game_board):
-    room_descriptions_pool = {1: "placeholder", 2: "placeholder", 3: "placeholder"}
+    room_descriptions_pool = {0: "placeholder", 1: "placeholder", 2: "placeholder", 3: "placeholder", 4: "placeholder"}
     room_key = game_board[character["coordinates"]]
     print(room_descriptions_pool[room_key])
 
