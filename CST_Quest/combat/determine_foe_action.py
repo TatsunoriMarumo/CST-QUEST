4e08c93da458bd27cbd8046f1614664e45df969d
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 15:05:00 -0800

    polish determine_foe_action() function

--- CST_Quest/combat/determine_foe_action.py
+++ CST_Quest/combat/determine_foe_action.py
@@ -9,10 +9,18 @@ import random
 
 def determine_foe_action(foe):
     if foe["stats"]["current_hp"] == foe["stats"]["max_hp"]:
-        foe_actions = {"1": "light_attack", "2": "heavy_attack"}
-        foe_action = str(random.choice(range(1, 3)))
-        return foe_actions[foe_action]
+        attack_type = ("light_attack", "heavy_attack")
+        foe_action = random.choice(range(1, 100))
+        if foe_action > 70:
+            return attack_type[1]
+        else:
+            return attack_type[0]
     else:
-        foe_actions = {"1": "light_attack", "2": "heavy_attack", "3": "block"}
-        foe_action = str(random.choice(range(1, 4)))
-        return foe_actions[foe_action]
+        action_type = ("light_attack", "heavy_attack", "block")
+        foe_action = random.choice(range(100))
+        if foe_action > 80:
+            return action_type[2]
+        elif 60 < foe_action < 80:
+            return action_type[1]
+        else:
+            return action_type[0]
