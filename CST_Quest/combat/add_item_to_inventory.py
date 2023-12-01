9412d95f11f68f12469577523a58a128a27bdf74
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 18:50:07 -0800

    create add_item_to_inventory() function

---
+++ CST_Quest/combat/add_item_to_inventory.py
@@ -0,0 +1,11 @@
+"""
+Tatsunori Marumo
+A01327744
+Ephraim Hsu
+A01366848
+"""
+
+
+def add_item_to_inventory(character, item):
+    character["inventory"] += item
+    print(f"{item} added to inventory!")
