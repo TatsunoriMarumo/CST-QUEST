dc96d8ffc05de29415de9770adc9b8a4826c8f95
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 18:40:30 -0800

    create calculate_attack_vs_defense() function

--- CST_Quest/combat/calculate_damage.py
+++ CST_Quest/combat/calculate_damage.py
@@ -8,4 +8,17 @@ import random
 
 
 def calculate_attack_vs_defense(attacking, attack_type, defending):
-    pass
+    attack_stat = attacking["stats"]["intelligence"]
+    block_stat = defending["stats"]["intelligence"]
+    if attack_type == "heavy_attack":
+        true_damage = random.choice(range(attack_stat * 1.2 - 3, attack_stat * 1.2)) - block_stat
+        if true_damage > 0:
+            defending["stats"]["current_hp"] -= true_damage
+        else:
+            print("Attack was blocked!")
+    else:
+        true_damage = random.choice(range(attack_stat - 1, attack_stat + 2))
+        if true_damage > 0:
+            defending["stats"]["current_hp"] -= true_damage
+        else:
+            print("Attack was blocked!")
