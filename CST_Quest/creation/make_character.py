1a7c67e888323135d8535824d5609c51f3281b70
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-11-30 18:45:27 -0800

    add effect_counter as item in character dictionary

--- CST_Quest/creation/make_character.py
+++ CST_Quest/creation/make_character.py
@@ -13,15 +13,15 @@ def create_character(user_name):
         character = {"name": user_name, "class": user_class, "level": 1, "exp": 0,
                      "stats": {"max_hp": 1, "current_hp": 1, "intelligence": 1, "luck": 1,
                                "typing_speed": 1, "mentality": 1},
-                     "coordinates": [0, 0], "inventory": []}
+                     "coordinates": [0, 0], "inventory": [], "effect_counter": {"buff": 0, "debuff": 0}}
     elif user_class == "back_end_developer":
         character = {"name": user_name, "class": user_class, "level": 1, "exp": 0,
                      "stats": {"max_hp": 2, "current_hp": 2, "intelligence": 2, "luck": 2,
                                "typing_speed": 2, "mentality": 2},
-                     "coordinates": [0, 0], "inventory": []}
+                     "coordinates": [0, 0], "inventory": [], "effect_counter": {"buff": 0, "debuff": 0}}
     else:
         character = {"name": user_name, "class": user_class, "level": 1, "exp": 0,
                      "stats": {"max_hp": 3, "current_hp": 3, "intelligence": 3, "luck": 3,
                                "typing_speed": 3, "mentality": 3},
-                     "coordinates": [0, 0], "inventory": []}
+                     "coordinates": [0, 0], "inventory": [], "effect_counter": {"buff": 0, "debuff": 0}}
     return character
