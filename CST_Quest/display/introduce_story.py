3f19e9fafd75c0c682b9fa45af981b486b1940ea
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-12-07 17:05:32 -0800

    complete introduce_story()

---
+++ CST_Quest/display/introduce_story.py
@@ -0,0 +1,34 @@
+def story_introduction():
+    """
+    Print the CST Quest introduction
+
+    :post-condition: print the story introduction and 'CST Quest' as ascii art
+    """
+    print(f"\nYou've been summoned to a mysterious realm known as The Techlands Between...\n\nUsing the last of "
+          f"their resources, and forbidden dark arts, the residents of The Techlands Between\ncall upon YOU, "
+          f"a destined hero, to show them the path forward. Ever since 'The Shattering', locally\nreferred to as "
+          f"tech industry layoffs, a deadly curse has spread across the realm. It is known as ... ChatGPT!\n"
+          f"For centuries, the Realms warriors have been trained in the programming arts; however, after being "
+          f"infected\nby the GPT curse, warriors have lost their ability to write original and home brewed code. "
+          f"The leaders of\nThe Techlands Between have decided to send you, our best chance of redemption, to ... The "
+          f"BCIT Dungeon!\n\nDun Dun Dunnnnn.\n\nThis guantlet of 6 floors and bosses have been known to "
+          f"mentally break even the strongest and fastest of programmers\nbut the fate of The Techlands Between"
+          f"rest on your shoulders. It is time for you to save us all and depart on your...\n"
+          f"""\
+            
+             ██████╗███████╗████████╗     ██████╗ ██╗   ██╗███████╗███████╗████████╗
+            ██╔════╝██╔════╝╚══██╔══╝    ██╔═══██╗██║   ██║██╔════╝██╔════╝╚══██╔══╝
+            ██║     ███████╗   ██║       ██║   ██║██║   ██║█████╗  ███████╗   ██║   
+            ██║     ╚════██║   ██║       ██║▄▄ ██║██║   ██║██╔══╝  ╚════██║   ██║   
+            ╚██████╗███████║   ██║       ╚██████╔╝╚██████╔╝███████╗███████║   ██║   
+             ╚═════╝╚══════╝   ╚═╝        ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝   ╚═╝   
+                                                                                                                                                                                  
+          """)
+
+
+def main():
+    story_introduction()
+
+
+if __name__ == "__main__":
+    main()
