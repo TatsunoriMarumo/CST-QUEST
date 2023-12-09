91440fd803d60c68ba692701e958530ad9db4598
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-12-09 11:35:42 -0800

    write docstring for create_foe()

--- creation/create_enemy.py
+++ creation/create_enemy.py
@@ -6,16 +6,13 @@ A01366848
 """
 
 
-def create_burnaby_student(character):
-    row = character["coordinates"][0]
-    column = character["coordinates"][1]
-    board_lower_bound = 4
-    board_higher_bound = 6
-    if board_lower_bound < row < board_higher_bound and board_lower_bound < column < 10:
-        return {"name": "year 1 Burnaby campus student",
-                "stats": {"max_hp": 1, "current_hp": 1, "intelligence": 1, "luck": 1,
-                          "typing_speed": 1, "mentality": 1}}
-    else:
-        return {"name": "year 1 Burnaby campus student",
-                "stats": {"max_hp": 1, "current_hp": 1, "intelligence": 1, "luck": 1,
-                          "typing_speed": 1, "mentality": 1}}
+def create_burnaby_student():
+    """
+    Create a foe, also known as Burnaby campus student
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "year 1 Burnaby campus student",
+            "stats": {"current_hp": 1, "intelligence": 1, "mental_fortitude": 1,
+                      "typing_speed": 1, "luck": 1}}
