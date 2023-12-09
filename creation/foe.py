5b2895442c82ce1a4dd38b200c12524b627dc26c
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-12-09 12:10:13 -0800

    Create foes

--- creation/foe.py
+++ creation/foe.py
@@ -6,7 +6,7 @@ A01366848
 """
 
 
-def create_foe():
+def create_foe_year_one():
     """
     Create a foe, also known as Burnaby campus student
 
@@ -15,4 +15,64 @@ def create_foe():
     """
     return {"name": "year 1 Burnaby campus student",
             "stats": {"current_hp": 1, "intelligence": 1, "mental_fortitude": 1,
-                      "typing_speed": 1, "luck": 1}}
+                      "typing_speed": 1, "luck": 1, "exp": 1}}
+
+
+def create_foe_year_two():
+    """
+    Create a foe, also known as Burnaby campus student
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "year 2 Burnaby campus student",
+            "stats": {"current_hp": 3, "intelligence": 3, "mental_fortitude": 3,
+                      "typing_speed": 3, "luck": 3, "exp": 3}}
+
+
+def create_foe_level_international():
+    """
+    Create a foe, also known as Burnaby campus student
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "international Burnaby campus student",
+            "stats": {"current_hp": 5, "intelligence": 7, "mental_fortitude": 7,
+                      "typing_speed": 3, "luck": 1, "exp": 5}}
+
+
+def create_foe_elite():
+    """
+    Create a foe, also known as Burnaby campus student
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "elite Burnaby campus student",
+            "stats": {"current_hp": 7, "intelligence": 7, "mental_fortitude": 7,
+                      "typing_speed": 7, "luck": 7, "exp": 7}}
+
+
+def create_foe_set_rep():
+    """
+    Create a foe, also known as Burnaby campus student
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Burnaby campus set rep",
+            "stats": {"current_hp": 10, "intelligence": 10, "mental_fortitude": 10,
+                      "typing_speed": 10, "luck": 10, "exp": 10}}
+
+
+def create_foe_graduate():
+    """
+    Create a foe, also known as Burnaby campus student
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Burnaby campus graduate",
+            "stats": {"current_hp": 13, "intelligence": 13, "mental_fortitude": 13,
+                      "typing_speed": 13, "luck": 13, "exp": 13}}
