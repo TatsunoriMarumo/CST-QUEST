f8a6d86e473cd8b22a83006d219fd318c4392992
Tatsunori Marumo <tmarumo@my.bcit.ca>
2023-12-09 12:21:10 -0800

    create bosses

--- creation/foe.py
+++ creation/foe.py
@@ -76,3 +76,75 @@ def create_foe_graduate():
     return {"name": "Burnaby campus graduate",
             "stats": {"current_hp": 13, "intelligence": 13, "mental_fortitude": 13,
                       "typing_speed": 13, "luck": 13, "exp": 13}}
+
+
+def create_foe_julian():
+    """
+    Create a foe, also known as an instructor, Julian
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Julian",
+            "stats": {"current_hp": 3, "intelligence": 3, "mental_fortitude": 3,
+                      "typing_speed": 3, "luck": 3}}
+
+
+def create_foe_sam():
+    """
+    Create a foe, also known as an instructor, Sam
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Sam",
+            "stats": {"current_hp": 5, "intelligence": 5, "mental_fortitude": 5,
+                      "typing_speed": 5, "luck": 5}}
+
+
+def create_foe_hoda():
+    """
+    Create a foe, also known as an instructor, Hoda
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Hoda",
+            "stats": {"current_hp": 7, "intelligence": 7, "mental_fortitude": 7,
+                      "typing_speed": 7, "luck": 7}}
+
+
+def create_foe_nabil():
+    """
+    Create a foe, also known as an instructor, Nabil
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Nabil",
+            "stats": {"current_hp": 11, "intelligence": 11, "mental_fortitude": 11,
+                      "typing_speed": 11, "luck": 11}}
+
+
+def create_foe_maryam():
+    """
+    Create a foe, also known as an instructor, Maryam
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Maryam",
+            "stats": {"current_hp": 13, "intelligence": 13, "mental_fortitude": 13,
+                      "typing_speed": 13, "luck": 13}}
+
+
+def create_foe_chris():
+    """
+    Create a foe, also known as an instructor, Chris
+
+    :postcondition: create a dictionary representing a foe
+    :return: a dictionary representing a foe
+    """
+    return {"name": "Chris",
+            "stats": {"current_hp": 17, "intelligence": 17, "mental_fortitude": 17,
+                      "typing_speed": 17, "luck": 17}}
