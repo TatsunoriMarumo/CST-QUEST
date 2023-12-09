9794113508d6b4473344193ec98395a76d923cca
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-12-09 08:00:56 -0800

    polish get_class() docstring

--- creation/character_creation.py
+++ creation/character_creation.py
@@ -12,7 +12,7 @@ def get_username():
     Ask player for their username.
 
     :postcondition: assign variable to player's selected username
-    :return: a string
+    :return: a variable with a string value
     :raises ValueError: if the user input only contains white space
     """
     user_name = input("Please enter your name:\n")
@@ -22,31 +22,27 @@ def get_username():
         return user_name.strip()
 
 
-def choose_class():
+def get_class():
     """
-    Let player choose a class
+    Ask player to choose their character class.
 
-    This function ask player to choose one class from three classes
-
-    postcondition: no invalid input is accepted
-    :return: the chosen user class stored as string
-    :raises ValueError: if user input is not string 1, 2 ,or 3
+    :precondition: player has chosen a username
+    :postcondition: assign variable to player's selected class
+    :return: a variable with a string value
+    :raises ValueError: if the user input is not 1, 2, or 3
     """
     while True:
-        try:
-            user_class = (input("choose a class\n1: front-end developer\n2: back-end developer\n3: full-stack developer\n")
-                          .strip())
+        user_class = (input("1: front-end developer\n2: back-end developer\n"
+                            "3: full-stack developer\nPlease choose a developer class using numbers: ").strip())
 
-            if user_class not in ["1", "2", "3"]:
-                raise ValueError("Invalid input: Please choose a valid class number")
-            if user_class == "1":
-                return "front_end_developer"
-            elif user_class == "2":
-                return "back_end_developer"
-            elif user_class == "3":
-                return "full_stack_developer"
-        except ValueError as e:
-            print("{}".format(str(e)), file=sys.stderr)
+        if user_class not in ["1", "2", "3"]:
+            raise ValueError("Invalid input, Please choose a valid class number.")
+        if user_class == "1":
+            return "front_end_developer"
+        elif user_class == "2":
+            return "back_end_developer"
+        else:
+            return "full_stack_developer"
 
 
 def assign_character_starting_stats(character, user_class):
@@ -79,7 +75,8 @@ def create_character():
             print("{}".format(str(e)), file=sys.stderr)
         else:
             user_class = choose_class()
-            character = {"name": user_name, "class": user_class, "level": 1, "exp": 0, "status": None, "coordinates": [0, 0],
+            character = {"name": user_name, "class": user_class, "level": 1, "exp": 0, "status": None,
+                         "coordinates": [0, 0],
                          "inventory": []}
 
             assign_character_starting_stats(character, user_class)
@@ -88,7 +85,7 @@ def create_character():
 
 
 def main():
-    get_username()
+    choose_class()
 
 
 if __name__ == '__main__':
