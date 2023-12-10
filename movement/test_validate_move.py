8e01de1c7ff1fc69a65ebb51c20335d0c52a5cf8
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-12-09 19:37:31 -0800

    write unit tests for validate_move()

--- movement/test_validate_move.py
+++ movement/test_validate_move.py
@@ -5,10 +5,38 @@ Ephraim Hsu
 A01366848
 """
 from unittest import TestCase
-
 from character_movement import validate_move
 
 
 class Test(TestCase):
-    def test_validate_move(self):
+    def test_validate_move_valid_movement_North(self):
+        board = {(1, 0): 1, (0, 0): 1}
+        character = {"coordinates": [1, 0]}
+        direction = "north"
+        actual = validate_move(board, character, direction)
+        expected = True
+        self.assertEqual(actual, expected)
 
+    def test_validate_move_valid_movement_East(self):
+        board = {(0, 0): 1, (0, 1): 1}
+        character = {"coordinates": [0, 0]}
+        direction = "east"
+        actual = validate_move(board, character, direction)
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_validate_move_valid_movement_South(self):
+        board = {(0, 0): 1, (1, 0): 1}
+        character = {"coordinates": [0, 0]}
+        direction = "south"
+        actual = validate_move(board, character, direction)
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_validate_move_valid_movement_West(self):
+        board = {(0, 1): 1, (0, 0): 1}
+        character = {"coordinates": [0, 1]}
+        direction = "west"
+        actual = validate_move(board, character, direction)
+        expected = True
+        self.assertEqual(actual, expected)
