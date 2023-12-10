cd86761d1539254f28470d80f35124a8ed1be194
Ephraim Hsu <ehsu18@my.bcit.ca>
2023-12-09 20:19:29 -0800

    write unit tests for ask_quiz_week_nine()

---
+++ quiz/test_quiz_week_nine.py
@@ -0,0 +1,40 @@
+"""
+Tatsunori Marumo
+A01327744
+Ephraim Hsu
+A01366848
+"""
+from unittest import TestCase
+from unittest.mock import patch
+from quiz_comp_1510 import ask_quiz_week_nine
+
+
+class Test(TestCase):
+    @patch("builtins.input", side_effect="!")
+    def test_ask_quiz_week_nine_invalid_input(self, _):
+        with self.assertRaises(ValueError):
+            ask_quiz_week_nine()
+
+    @patch("builtins.input", side_effect="1")
+    def test_ask_quiz_week_nine_valid_input_one(self, _):
+        actual = ask_quiz_week_nine()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    @patch("builtins.input", side_effect="2")
+    def test_ask_quiz_week_nine_valid_input_two(self, _):
+        actual = ask_quiz_week_nine()
+        expected = True
+        self.assertEqual(actual, expected)
+
+    @patch("builtins.input", side_effect="3")
+    def test_ask_quiz_week_nine_valid_input_three(self, _):
+        actual = ask_quiz_week_nine()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    @patch("builtins.input", side_effect="4")
+    def test_ask_quiz_week_nine_valid_input_four(self, _):
+        actual = ask_quiz_week_nine()
+        expected = False
+        self.assertEqual(actual, expected)
